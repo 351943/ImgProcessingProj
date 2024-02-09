fcfba88454ad6e02b7534031f7c70443281c3307
el <elai146@student.fuhsd.org>
2024-02-08 19:53:25 -0800

    completed displaymethod

--- src/Filters/DisplayInfoFilter.java
+++ src/Filters/DisplayInfoFilter.java
@@ -3,8 +3,6 @@ package Filters;
 import Interfaces.PixelFilter;
 import core.DImage;
 
-import java.sql.SQLOutput;
-
 public class DisplayInfoFilter implements PixelFilter {
     public DisplayInfoFilter() {
         System.out.println("Filter running...");
@@ -14,17 +12,41 @@ public class DisplayInfoFilter implements PixelFilter {
     public DImage processImage(DImage img) {
         short[][] grid = img.getBWPixelGrid();
 
-        System.out.println("Image is " + grid.length + " by "+ grid[0].length);
-        img.setPixels(grid);
+        grid = crop(grid, 0, 0, 500, 500);
 
-        int bubbleSize = (221-102)/5;
-        int r = 109;
-        for (int c = 102; c < 221; c+=bubbleSize) {
-            //displayBubbleBorder(grid, r, c, bubbleSize);
-            int percent = getPercentageFilled(grid,r,c,bubbleSize);
+        System.out.println("Image is " + grid.length + " by "+ grid[0].length);
+
+        int bubbleSize = (224-104)/5;
+        System.out.println(bubbleSize);
+        int r = 106;
+        for (int c = 104; c < 104+bubbleSize*5; c+=bubbleSize) {
+            //int percent = getPercentageFilled(grid,r,c,bubbleSize);
+            displayBubbleBorder(grid, r, c, bubbleSize);
         }
 
+        img.setPixels(grid);
         return img;
     }
+
+    private void displayBubbleBorder(short[][] grid, int r, int c, int bubbleSize) {
+        for (int i = r; i < r+bubbleSize; i++) {
+            for (int j = c; j < c+bubbleSize; j++) {
+                if(i==r||j==c||i==r+bubbleSize-1||j==c+bubbleSize-1){
+                    grid[i][j]=0;
+                }
+            }
+        }
+    }
+
+    private short[][] crop(short[][] grid, int r, int c, int r1, int c1) {
+        short[][] newGrid = new short[r1][c1];
+        for (int i = r; i < r1; i++) {
+            for (int j = c; j < c1; j++) {
+                newGrid[i][j]=grid[i][j];
+            }
+        }
+        return newGrid;
+    }
+
 }
 
